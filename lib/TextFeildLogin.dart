6f801e866e580a7aa01ca81fa7c745f63ac52de4
thichu <thichu08@gmail.com>
2021-02-24 14:38:46 +0700

    vẽ lại textField

--- lib/TextFeildLogin.dart
+++ lib/TextFeildLogin.dart
@@ -9,30 +9,71 @@ class TextFeildLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     Size mSize = MediaQuery.of(context).size;
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: mSize.width / 50),
+      padding: EdgeInsets.only(left: mSize.width / 30, right: mSize.width / 50),
+      margin: EdgeInsets.only(bottom: mSize.height / 80),
+      width: double.infinity,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(5),
       ),
       child: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: "enter login",
-              hintTextDirection: TextDirection.rtl,
-            ),
-            style: TextStyle(
-              fontFamily: "Roboto",
-            ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: mSize.width / 80),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w700,
+                    fontSize: mSize.height / 36,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "enter login",
+                    hintTextDirection: TextDirection.rtl,
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(
+                      fontSize: mSize.height / 36,
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "email",
-              hintTextDirection: TextDirection.rtl,
-            ),
-            style: TextStyle(
-              fontFamily: "Roboto",
-            ),
+          Divider(
+            height: mSize.width / 80,
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: mSize.width / 80),
+                child: Text(
+                  "Email",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w700,
+                    fontSize: mSize.height / 36,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "email",
+                      hintTextDirection: TextDirection.rtl,
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(
+                          fontSize: mSize.height / 36, fontFamily: "Roboto")),
+                ),
+              ),
+            ],
           ),
         ],
       ),
